9e8fded4990e8cc18904882e8c725951a935304d
DeAn0nim <128257558+RetrO-M@users.noreply.github.com>
2025-03-19 06:17:05 +0100

    🛜 UPDATE

--- setup.sh
+++ setup.sh
@@ -1,57 +1,83 @@
 #!/bin/bash
+
 RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
 NC='\033[0m' 
+
 title() {
-    echo -e """${NC}
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⣿⣆⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⠁${RED}•${NC} ⢿⣿⡿⣿⣿⡆⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣦⣤⣴⣿⠃${RED}•${NC} ⣿⡇⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⡿⠋⠁⣿⠟⣿⣿⢿⣧⣤⣴⣿⡇⠀
-⠀⠀⠀⠀⢀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠘⠁⢸⠟⢻⣿⡿⠀⠀
-⠀⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣴⣇⢀⣤⠀⠀⠀⠀⠘⣿⠃⠀⠀
-⠀⠀⠀⠀⠀⢈⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣴⣿⢀⣴⣾⠇⠀⠀⠀
-⠀⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀
-⠀⠀⠉⠉⠉⠉⣡⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⡿⠟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀
-⠀⠀⣴⡾⠿⠿⠿⠛⠋⠉⠀⢸⣿⣿⣿⣿⠿⠋⢸⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⡿⠟⠋⠁⠀⠀⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠈⠀
-${NC}[${RED}01${NC}] Install Modules
-${NC}[${RED}02${NC}] Install Python
+    clear
+    echo -e """${CYAN}
+         ▄▄▄▄▄▄▄▄
+        ▄████████████
+      ▄██   ████▀▀████
+  ▐▄ ▐███▄ ▄███▌   ███▌▄██
+   █▌████████████▄███████▌
+   █████████ ▐██████████▀
+     ██████████████████
+      ████████████████
+       ▀██████████████
+         ▀█████████████▄
+            ▀▀▀██████████ 
+${NC}
+${BLUE}Setup - GhostSniffer${NC}
+
+[${CYAN}01${NC}] Install Python modules
+[${CYAN}02${NC}] Check and install Python3
+[${CYAN}03${NC}] Update system dependencies
+[${CYAN}99${NC}] Help
+[${CYAN}00${NC}] Exit
     """
 }
+
 setup_modules() {
-    pip3 install scapy
-    pip3 install colorama
+    echo -e "${NC}[${BLUE}*${NC}] Installing Python modules...${NC}"
+    pip3 install --upgrade scapy colorama scapy
+    echo -e "${NC}[${CYAN}✓${NC}] Modules installed successfully.${NC}"
 }
+
 setup_python() {
     if command -v python3 &>/dev/null; then
-        echo -e "${GREEN}[+]${NC} Python is already installed."
+        echo -e "${NC}[${CYAN}✓${NC}] Python3 is already installed.${NC}"
     else
-        echo -e "${RED}[+]${NC} Python is not installed. Installing..."
-
-        sudo apt update
-        sudo apt install python3 -y
+        echo -e "${NC}[${RED}!${NC}] Python3 is not installed. Installing...${NC}"
+        sudo apt update && sudo apt install python3 python3-pip -y
+        echo -e "${NC}[${CYAN}✓${NC}] Python3 installed successfully.${NC}"
     fi
 }
-main() {
-    title
-    read -p "root:~# " choice
 
-    case $choice in
-        1)
-            setup_modules
-            ;;
-        2)
-            setup_python
-            ;;
-        *)
-            echo -e "${RED}[+]${NC} Invalid option"
-            ;;
-    esac
+update_dependencies() {
+    echo -e "${NC}[${BLUE}*${NC}] Updating system packages...${NC}"
+    sudo apt update && sudo apt upgrade -y
+    echo -e "${NC}[${CYAN}✓${NC}] System updated successfully.${NC}"
 }
+
+help() {
+    echo -e """
+${CYAN} █  █  █▀▀▀  █     █▀▀█ 
+${NC} █▀▀█  █▀▀▀  █     █▄▄█ 
+${CYAN} █  █  █▄▄▄  █▄▄█  █
+    """
+    echo -e "${NC}[${CYAN}✓${NC}] Command: ${CYAN}'python3 main.py -i <INTERFACE>'"
+}
+
+main() {
+    while true; do
+        title
+        read -p "Setup> " choice
+
+        case $choice in
+            1) setup_modules ;;
+            2) setup_python ;;
+            3) update_dependencies ;;
+            99) help ;;
+            0) echo -e "${NC}[${CYAN}+${NC}] Bye bye...${NC}"; exit 0 ;;
+            *) echo -e "${NC}[${RED}X${NC}] Invalid option, try again.${NC}" ;;
+        esac
+
+        echo -e "\n${NC}[${CYAN}-${NC}] Press Enter to return to the menu...${NC}"
+        read
+    done
+}
+
 main
